1cc8547df95ad0e95daa5bd4a9b14d4c62645bf9
Laurent Bernabe <laurent.bernabe@gmail.com>
2021-04-29 11:30:00 +0200

    Trying to fix linux build #3

--- README.md
+++ README.md
@@ -6,3 +6,9 @@ Choose an exercise from your chess pgn file and train yourself against the devic
 
 * First, you must set up the engine path in the options page : it must be an UCI-compliant engine (i.e Stockfish).
 * Then when starting a game, you will be invited to choose a Pgn file, the game and the players configuration, as well as the time configuration.
+
+## Developers
+
+1. Setup configuration for your OS : https://tauri.studio/en/docs/getting-started/intro
+2. `npm install` or `yarn` in order to install dependencies locally (first time).
+4. `npm run serve` or `yarn serve` for a dev environment.
--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -1,48 +1,44 @@
 <template>
+  <div>
     <loloof64-chessboard
-        class="chessboard"
-        ref="chessboard"
-        size="400"
-        background="crimson"
-        coordinates_color="yellow"
-        white_cell_color="navajowhite"
-        black_cell_color="peru"
-        reversed="false"
-        origin_cell_color="red"
-        target_cell_color="green"
-        dnd_cross_color="purple"
-        white_player_human="true"
-        black_player_human="true"
-        move_highlight_color="red"
-        last_move_visible="true"
-        coordinates_visible="true"
+      class="chessboard"
+      ref="chessboard"
+      size="400"
+      background="crimson"
+      coordinates_color="yellow"
+      white_cell_color="navajowhite"
+      black_cell_color="peru"
+      reversed="false"
+      origin_cell_color="red"
+      target_cell_color="green"
+      dnd_cross_color="purple"
+      white_player_human="true"
+      black_player_human="true"
+      move_highlight_color="red"
+      last_move_visible="true"
+      coordinates_visible="true"
     ></loloof64-chessboard>
     <button @click="startGame">New game</button>
     <button @click="stopGame">Stop game</button>
+  </div>
 </template>
 
 <script>
-import {ref} from 'vue';
+import { ref } from "vue";
 
 export default {
-    setup() {
-        const chessboard = ref(null);
+  setup() {
+    const chessboard = ref(null);
 
-        const startGame = () => {
-            if (chessboard.value != null) chessboard.value.newGame();
-        }
+    const startGame = () => {
+      if (chessboard.value != null) chessboard.value.newGame();
+    };
 
-        const stopGame = () => {
-            if (chessboard.value != null) chessboard.value.stop();
-        }
+    const stopGame = () => {
+      if (chessboard.value != null) chessboard.value.stop();
+    };
 
-        return {chessboard, startGame, stopGame};
-    }
-}
+    return { chessboard, startGame, stopGame };
+  },
+};
 </script>
-
-<style scoped>
-.chessboard {
-    display: inline-block;
-}
-</style>
